bb37264032de1e22796e6e29ce74d5394841af22
albin <albina.petruseva@yandex.ru>
2022-05-22 20:40:11 +0300

    2_2_3

--- src/main/java/crud/Service/ServiceUser.java
+++ src/main/java/crud/Service/ServiceUser.java
@@ -1,7 +1,7 @@
 package crud.Service;
 
+import crud.dao.DaoUser;
 import crud.model.User;
-import crud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,43 +9,40 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-public class ServiceUser implements ServiceInterfaceUser {
+public class ServiceUser implements ServiceInterface {
     @Autowired
-    private UserRepository userRepository;
+    private DaoUser daoUser;
 
     @Transactional
     @Override
     public void save(User user) {
-        userRepository.save(user);
+        daoUser.save(user);
     }
 
     @Transactional
     @Override
     public void updateUser(User user, int id) {
-        User user1 = userRepository.findById(id).get();
-        user1.setName(user.getName());
-        user1.setAge(user.getAge());
-        user1.setHobby(user.getHobby());
+        daoUser.updateUser(user, id);
 
     }
 
     @Transactional
     @Override
     public void deleteUser(int id) {
-        userRepository.deleteById(id);
+        daoUser.deleteUser(id);
 
     }
 
     @Transactional
     @Override
     public User findUser(int id) {
-        User user = userRepository.findById(id).get();
+        User user = daoUser.findUser(id);
         return user;
     }
 
     @Transactional
     @Override
     public List<User> allUsers() {
-        return userRepository.findAll();
+        return daoUser.allUsers();
     }
 }
